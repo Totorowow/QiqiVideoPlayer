f3bd015b26d73eed72370daf8e0bfc59a65843fc
summer_squirrel <85954091@qq.com>
2023-01-06 00:29:57 +0800

    update code

--- QiqiVideoPlayerDemo/src/main/java/com/woodpecker/qiqivideoplayer/newPlayer/surface/TestSurfaceActivity.java
+++ QiqiVideoPlayerDemo/src/main/java/com/woodpecker/qiqivideoplayer/newPlayer/surface/TestSurfaceActivity.java
@@ -1,49 +1,60 @@
 package com.woodpecker.qiqivideoplayer.newPlayer.surface;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.woodpecker.qiqivideoplayer.ConstantVideo;
 import com.woodpecker.qiqivideoplayer.R;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class TestSurfaceActivity extends AppCompatActivity {
+public class TestSurfaceActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private MySurfaceView mSurfaceView;
+    private QiqiSurfaceView surfaceView;
+    private Button btn_play,btn_pause,btn_stop;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        surfaceView = findViewById(R.id.surfaceView);
+        btn_play = findViewById(R.id.btn_play);
+        btn_play.setOnClickListener(this);
+        btn_pause = findViewById(R.id.btn_pause);
+        btn_pause.setOnClickListener(this);
+        btn_stop = findViewById(R.id.btn_stop);
+        btn_stop.setOnClickListener(this);
 
-        FrameLayout fl = (FrameLayout) findViewById(R.id.fl);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            //播放，继续播放的按钮
+            case R.id.btn_play:
+                surfaceView.setDataPath(ConstantVideo.VideoPlayerList[3]);
+            case R.id.btn_pause:
+                surfaceView.playOrNo();
+                break;
+            //停止播放视频 后如果再次点击播放视频按钮 则视频是从头播放
+            case R.id.btn_stop:
+                surfaceView.stop();
+                break;
+            default:
+                break;
+        }
+    }
 
 
-        mSurfaceView = new MySurfaceView(this);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
-        fl.addView(mSurfaceView, params);
-
-        SurfaceHolder holder = mSurfaceView.getHolder();
-        //增加surfaceView的监听
-        holder.addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-            }
-        });
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        surfaceView.release();
     }
 }
